c732a3ff815cb64f6fe6f7941ff883b21ec067a1
Gigi Minova <minova.gigi@gmail.com>
2019-09-30 00:40:23 +0100

    display all data

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
-import Data from "../model/data";
+import Data from "../model/Data";
 
-const Quiz = ({ setPage, score, setScore  }) => {
-    console.log(score);
+const Quiz = ({ setPage, score, setScore }) => {
   return (
     <div>
       <h1>Quizz page</h1>
-      <button onClick={() => setScore(Number(score)+1)}>Score +1!</button>
+      {Data.map((q, i) => (
+        <p key={i}>
+          #{i + 1}: {q.question}
+        </p>
+      ))}
+      <button onClick={() => setScore(Number(score) + 1)}>Score +1!</button>
       <button onClick={() => setPage("Score")}>Finish</button>
     </div>
   );
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -14,7 +14,7 @@ const WelcomePage = ({ setPage, score, setScore, username, setUsername }) => {
           className="input-username"
           onChange={updateUsername}
         />
-        <p className="display-username">You're playing as: {username}</p>
+        <p className="display-username">You're playing as: {username.slice(0,1).toUpperCase().concat(username.slice(1))}</p>
         <button className="button--start" onClick={() => setPage("Quiz")}>
           Let's start!
         </button>
