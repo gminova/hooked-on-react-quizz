3a57faeb8fa9fb40a8cc4de4f7d0af78d01cd864
Gigi Minova <minova.gigi@gmail.com>
2019-09-29 15:41:02 +0100

    basic data struct

---
+++ src/model/data.js
@@ -0,0 +1,32 @@
+const Data = [
+  {
+    question: "Do you like React?",
+    answers: [
+      {
+        "not at all": false
+      },
+      {
+        "a bit": false
+      },
+      {
+        "hell, yeah": true
+      }
+    ]
+  },
+  {
+    question: "Do you like Angular?",
+    answers: [
+      {
+        "not at all": true
+      },
+      {
+        "a bit": false
+      },
+      {
+        "hell, yeah": false
+      }
+    ]
+  }
+];
+
+export default Data;
