1274d4b34bfe4b1adeb5e26e4dd527bcceb68f70
Gigi Minova <minova.gigi@gmail.com>
2019-09-29 20:13:10 +0100

    add small function to capitalise username

--- src/components/Score.js
+++ src/components/Score.js
@@ -3,7 +3,7 @@ import React from "react";
 const Score = ({ setPage, username, score }) => {
   return (
     <div>
-      <h1>Well done, {username}, your score is: {score}</h1>
+      <h1>Well done, {username.slice(0,1).toUpperCase().concat(username.slice(1))}, your score is: {score}</h1>
       <button onClick={() => setPage("WelcomePage")}>Play again!</button>
     </div>
   );
