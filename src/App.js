bd270ced581ffef5e470bc1de34c60f9f027ceed
Gigi Minova <minova.gigi@gmail.com>
2019-09-29 17:12:51 +0100

    add score tracker

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import Score from "./components/Score";
 
 function App() {
   const [page, setPage] = useState("WelcomePage");
+  const [score, setScore] = useState("0");
   return (
     <>
       {page === "WelcomePage" && <WelcomePage setPage={setPage} />}
-      {page === "Quiz" && <Quiz setPage={setPage} />}
-      {page === "Score" && <Score setPage={setPage} />}
+      {page === "Quiz" && (
+        <Quiz setPage={setPage} score={score} setScore={setScore} />
+      )}
+      {page === "Score" && (
+        <Score setPage={setPage} score={score} setScore={setScore} />
+      )}
     </>
   );
 }
--- src/components/Score.js
+++ src/components/Score.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Score = ({ setPage }) => {
+const Score = ({ setPage, score, setScore }) => {
+  console.log(score);
   return (
     <div>
       <h1>Score page</h1>
+      <button onClick={() => setScore(() => (Number(score)+1))}>Score +1!</button>
       <button onClick={() => setPage("WelcomePage")}>Play again!</button>
     </div>
   );
