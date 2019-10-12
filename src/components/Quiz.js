4480c4e5f11bd516e6fd8f39b1640519ccd03d3a
Gigi Minova <minova.gigi@gmail.com>
2019-10-12 18:41:58 +0100

    render one question with answers at a time

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,21 +2,38 @@ import React, { useState, useEffect } from "react";
 import Data from "../model/Data";
 
 const Quiz = ({ setPage, score, setScore }) => {
+  const [count, setCount] = useState(0);
+  const [question, setQuestion] = useState(Data[count]);
   const [answer, setAnswer] = useState(false);
+  console.log({ question });
+
+  useEffect(() => {
+    const handleQuestion = () => {
+      return answer === true ? setCount(count + 1) : count;
+    };
+  }, [answer]);
+
   return (
     <div>
       <h1>Quizz page</h1>
-      {Data.map((q, i) => (
-        <>
-          <p key={i}>
-            #{i + 1}: {q.question}
-          </p>
-
-          {q.answers.map((a, i) => (
-              <button key={i}>{a.text}</button>
-          ))}
-        </>
-      ))}
+      <>
+        <p>{Data[count].question}</p>
+        {question.answers.map((a, i) => (
+          <div>
+            <input
+              type="radio"
+              value={a.text}
+              key={i}
+              id={a.text}
+              name="answer"
+              onChange={e => setAnswer(e.target.value)}
+            />
+            <label id={a.text} value={a.text} key={a.text}>
+              {a.text}
+            </label>
+          </div>
+        ))}
+      </>
       <button onClick={() => setScore(Number(score) + 1)}>Score +1!</button>
       <button onClick={() => setPage("Score")}>Finish</button>
     </div>
