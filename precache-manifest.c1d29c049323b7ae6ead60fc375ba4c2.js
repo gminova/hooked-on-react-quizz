7967ea127c27b8f3a173f953bc5e64c3ad386fb6
Gigi Minova <minova.gigi@gmail.com>
2019-10-13 20:32:16 +0100

    Updates

--- precache-manifest.c1d29c049323b7ae6ead60fc375ba4c2.js
+++ precache-manifest.c1d29c049323b7ae6ead60fc375ba4c2.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "49977073dccfde03c4ae983362aaba36",
+    "revision": "17873abb7ef8f11633ab7451f447ec9c",
     "url": "/hooked-on-react-quizz/index.html"
   },
   {
-    "revision": "3cfda90e890fe8fb20f0",
-    "url": "/hooked-on-react-quizz/static/css/main.cfda8877.chunk.css"
+    "revision": "af2c30ea8eb1f8904b17",
+    "url": "/hooked-on-react-quizz/static/css/main.a2a7298c.chunk.css"
   },
   {
     "revision": "3c108c6b392b5b57ea55",
     "url": "/hooked-on-react-quizz/static/js/2.7934ce2b.chunk.js"
   },
   {
-    "revision": "3cfda90e890fe8fb20f0",
-    "url": "/hooked-on-react-quizz/static/js/main.e0ccea4c.chunk.js"
+    "revision": "af2c30ea8eb1f8904b17",
+    "url": "/hooked-on-react-quizz/static/js/main.d7198939.chunk.js"
   },
   {
     "revision": "0111898a62e7c0a2e410",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hooked-on-react-quizz/precache-manifest.b8b257c4bdc1f85a361a4414bcf8dddc.js"
+  "/hooked-on-react-quizz/precache-manifest.c1d29c049323b7ae6ead60fc375ba4c2.js"
 );
 
 self.addEventListener('message', (event) => {
